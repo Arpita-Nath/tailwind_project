beed2e1223c19b03b5180b96c42a9e5f07e9af37
Arpita-Nath <arpitanath39@gmail.com>
2024-01-08 18:53:03 +0600

    navbar added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          navbg: '#0D0D0D',
+          black: '#1C1D28',
+          primary: '#FFD2A4',
+        },
+        maxWidth: {
+          'container': '1280px',
+        },
+        fontFamily: {
+          'roboto': "'Roboto Condensed', sans-serif",
+      }
+    }
+  }
+}
